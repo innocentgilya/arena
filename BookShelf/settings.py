1eb5a68696b26b103a16c8cc0465bda549d9220f
innocentgilya <163510318+innocentgilya@users.noreply.github.com>
2024-08-22 08:18:09 +0300

    new changes

--- BookShelf/settings.py
+++ BookShelf/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
@@ -139,7 +139,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+#STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 STATIC_ROOT = os.path.join(BASE_DIR,  'staticfiles',)
 
