b887cf568b8f1abbd7f81fda437c77c1fbbc2565
innocentgilya <163510318+innocentgilya@users.noreply.github.com>
2024-08-22 22:19:48 +0300

    Ready for deployment to Platform.sh.2

--- BookShelf/settings.py
+++ BookShelf/settings.py
@@ -184,3 +184,31 @@ ACCOUNT_USERNAME_REQUIRED =  False
 AUTH_USER_MODEL = 'userauths.User'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Platform.sh settings.
+from platformshconfig import Config
+
+config = Config()
+if config.is_valid_platform():
+    ALLOWED_HOSTS.append('.platformsh.site')
+    DEBUG = False
+
+    if config.appDir:
+        STATIC_ROOT = Path(config.appDir) / 'static'
+    if config.projectEntropy:
+        SECRET_KEY = config.projectEntropy
+
+    if not config.in_build():
+        db_settings = config.credentials('database')
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': db_settings['path'],
+                'USER': db_settings['username'],
+                'PASSWORD': db_settings['password'],
+                'HOST': db_settings['host'],
+                'PORT': db_settings['port'],
+    },
+}
